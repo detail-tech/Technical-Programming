21525aca84546f1e0bf2a3a3ae915b034af5c89c
Dikshant Sharma <sharmadikshant5705@gmail.com>
2024-10-25 09:15:25 +0530

    Most repeating character in given string

---
+++ Most repeating character.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //Most repeating character 
+    map<char,int>mp;
+    string s="aabbbcddd";
+    char first;
+    int cnt=0;
+    for(int i=0;i<s.length();i++){
+        mp[s[i]]++;
+    }
+    for(auto it:mp){
+        if(cnt<it.second){
+            first=it.first;
+            cnt=it.second;
+        }
+    }
+    cout<<first;
+    
+    return 0;
+}
