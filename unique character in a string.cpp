dc60d63fc045d5dd151fa9a092232e0abc19b262
Dikshant Sharma <sharmadikshant5705@gmail.com>
2024-10-29 16:10:53 +0530

    first unique character in a string

--- unique character in a string.cpp
+++ unique character in a string.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 
 int main() {
-    //unique character in a string
+    //first unique character in a string
     string s="Leetcode";
-    int cnt;
+    int cnt,index=0;
     for(int i=0;i<s.length();i++){
         cnt=0;
         for(int j=0;j<s.length();j++){
             if(s[i]==s[j])cnt++;
         }
         if(cnt==1){
-            cout<<s[i]<<endl;
+            index=i;
+            cout<<s[i];
+            break;
         }
     }
 
