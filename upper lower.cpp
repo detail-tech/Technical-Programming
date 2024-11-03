35296829a2524a0c370e083ffa224f864ce9eaf4
Dikshant Sharma <sharmadikshant5705@gmail.com>
2024-11-03 10:53:36 +0530

    Replace uppercase with '#' and lower case with '&'

---
+++ upper lower.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length();i++){
+        if(s[i]>='A' && s[i]<='Z')s[i]='#';
+        if(s[i]>='a' && s[i]<='z')s[i]='&';
+    }
+    cout<<s;
+
+    return 0;
+}
